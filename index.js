9283232e283ae30f13037b23bab36ef9b03db929
Abraham Scott <abrahamscott02@gmail.com>
2023-07-07 13:31:30 -0600

    Done

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(distanceInBlocks) {
+   return Math.abs(distanceInBlocks - 42);
+}
+
+function distanceFromHqInFeet(distanceInFeet){
+    return Math.abs(distanceFromHqInBlocks(distanceInFeet)*264);
+}
+
+function distanceTravelledInFeet(start, destination){
+    return Math.abs((start-destination)*264);
+}
+
+function calculatesFarePrice(start, destination){
+    let distance = distanceTravelledInFeet(start, destination);
+    if (distance <= 400){
+        return 0;
+    } else if (distance > 400 && distance <= 2000){
+        return Math.abs((distance-400)*0.02);
+    } else if (distance > 2000 && distance <= 2500){
+        return 25;
+    } else if (distance > 2500) {
+        return "cannot travel that far";
+    }
+}
